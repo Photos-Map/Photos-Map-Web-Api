ae278ee404f1778120d64c91b42057c42017f113
Emilio K <emiliokartono@gmail.com>
2024-08-02 23:02:40 -0700

    feat: setup repo with PNPM, TS, EsLint, Prettier, Jest, and github actions

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import ExpressConfig from './express.config'
+
+const app = ExpressConfig()
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => console.log('Server Running on Port' + PORT))
