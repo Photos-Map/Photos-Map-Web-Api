ae8a289e634cefcf4cfaa9664e2d914eb1a72dd5
Emilio K <emiliokartono@gmail.com>
2024-08-03 15:10:07 -0700

    feat: setup express server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,38 @@
-import ExpressConfig from './express.config'
+import Cluster from 'cluster'
+import Process from 'process'
+import dotenv from 'dotenv'
+import OS from 'os'
+import { App } from './app'
+import logger from './logger'
 
-const app = ExpressConfig()
-const PORT = process.env.PORT || 3000
+let numRetries = 10
 
-app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
+dotenv.config()
+
+if (Cluster.isPrimary) {
+  const numForks = Number(process.env.NUM_FORKS) || OS.cpus().length
+
+  for (let i = 0; i < numForks; i++) {
+    Cluster.fork()
+  }
+
+  // Fork the server again if it dies
+  Cluster.on('exit', (_) => {
+    logger.info('A worker has died!')
+    numRetries--
+
+    if (numRetries > 0) {
+      logger.info('Relaunching worker again')
+      Cluster.fork()
+    } else {
+      logger.info('Not launching worker again')
+    }
+  })
+} else {
+  logger.info(`Child process #${Process.pid} spawned`)
+
+  const app = new App({
+    port: parseInt(process.env.PORT || '3000')
+  })
+  app.run()
+}
