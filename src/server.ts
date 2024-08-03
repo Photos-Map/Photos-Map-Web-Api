5541243f4bae951eb384c817a8bdb674f5eb863f
Emilio K <emiliokartono@gmail.com>
2024-08-03 14:14:23 -0700

    feat: setup auto deploy

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import ExpressConfig from './express.config'
 const app = ExpressConfig()
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log('Server Running on Port' + PORT))
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
