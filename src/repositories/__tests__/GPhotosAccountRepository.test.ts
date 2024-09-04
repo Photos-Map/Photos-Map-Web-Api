3aa96210d2bfd4a9161e4af3e2d97e5ffc511b54
Emilio K <emiliokartono@gmail.com>
2024-09-04 01:29:14 -0700

    wip

---
+++ src/repositories/__tests__/GPhotosAccountRepository.test.ts
@@ -0,0 +1,9 @@
+describe('getGPhotosClient()', () => {
+  it('should return Google Photos client given correct parameters', async () => {})
+
+  it('should update credentials in MongoDB when Google Photos client updates credentials', async () => {})
+
+  it('should throw NoGPhotosAccountFound when no account is found', async () => {})
+
+  it('should return the same Google Photos client when the same account is requested more than once', async () => {})
+})
